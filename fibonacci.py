3f36869ea10298dc779a0b7415a2704ca28e40f0
albertusortiz <alberto.ortiz.vargas@gmail.com>
2020-11-25 13:10:56 -0600

    Creando archivo y definiendo el metodo main.

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: latin1
+
+##############################################################################################
+#                                                                                            #
+#       Genera una función que devuelva una secuencia de Fibonacci como una array            #
+#                                                                                            #
+##############################################################################################
+
+def metodo_fibonacci():
+    print("0 1 1 2 3 5 8 13 21 34 55 89 144")
+
+if __name__ == "__main__":
+    metodo_fibonacci()

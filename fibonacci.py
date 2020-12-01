bce25e0927a98167517f0766dc84d21078adf298
albertusortiz <alberto.ortiz.vargas@gmail.com>
2020-11-30 23:30:42 -0600

    Resolviendo problema de fibonacci.

--- fibonacci.py
+++ fibonacci.py
@@ -7,8 +7,30 @@
 #                                                                                            #
 ##############################################################################################
 
-def metodo_fibonacci():
-    print("0 1 1 2 3 5 8 13 21 34 55 89 144")
+import time
+import sys
+
+def fibonnaci_recursivo(n):
+    if n == 0 or n == 1:
+        return 1
+    return fibonnaci_recursivo(n - 1) + fibonnaci_recursivo(n - 2)
+
+def fibonnaci_dinamico(n, memo = {}):
+    if n == 0 or n == 1:
+        return 1
+    try:
+        return memo[n]
+    except KeyError as e:
+        resultado = fibonnaci_dinamico(n - 1, memo) + fibonnaci_dinamico(n - 2, memo)
+        memo[n] = resultado 
+        print(memo)
+        return resultado
 
 if __name__ == "__main__":
-    metodo_fibonacci()
+    sys.setrecursionlimit(1000002)
+    n = int(input('Escoge un numero: '))
+    comienzo = time.time()
+    resultado = fibonnaci_dinamico(n)
+    print(resultado)
+    final = time.time()
+    print(final - comienzo)
